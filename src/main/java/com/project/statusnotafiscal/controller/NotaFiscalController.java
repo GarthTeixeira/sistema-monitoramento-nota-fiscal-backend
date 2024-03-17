cf1bb45a848f1319378499624b504ee28dc6fd90
GArthTex66 <garthur1998@gmail.com>
2024-03-17 13:48:53 -0300

    Habilitando Cors para frontend

--- src/main/java/com/project/statusnotafiscal/controller/NotaFiscalController.java
+++ src/main/java/com/project/statusnotafiscal/controller/NotaFiscalController.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("api/v1/notas-fiscais")
 @AllArgsConstructor
